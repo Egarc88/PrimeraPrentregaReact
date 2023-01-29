66a878002aa685f082461c0b3f031d3c298e77a4
egarc40228 <erick007fernando@gmail.com>
2023-01-29 12:37:23 -0600

    Cambios Finales

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import NavBar from './Components/NavBar.jsx';
+import ItemListContainer from './Components/ItemListContainer.jsx';
+/* import Cover from './Components/cover.jsx' */
+import Categories from './Components/Categorias';
+
+import './Components/Styles/NavBar.css';
+import './Components/Styles/CartWiget.css';
+import './Components/Styles/ItemListContainer.css';
+import './Components/Styles/Categorias.css';
+import './Components/Styles/Cover.css';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <NavBar/>
+    
+    {/* <Cover/> */}
+    <ItemListContainer greeting="Nuestros Productos"/>
+    <Categories/>
+    </>
+
   );
 }
 
